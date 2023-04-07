eaedcb9ddc483da04fceeda206256d0c6bd7cfe4
beccabidus <beccabidus@gmail.com>
2023-04-06 23:30:08 -0400

    Updated javascript for working menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 console.log("Hello,World!")
 
-function menuToggle () {
-    var x = document.getElementById("myNavtoggle") ;
-    if (x.className === "navToggle") {
-    x.className += " responsive";
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+    x.className += ' responsive';
     } else {
-        x.className = "navtoggle";
+    x.className = 'navtoggle';
     }
-}
+    }
